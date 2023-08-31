a5ced80af5f2721a9f016c7f7a1ca855d9312ef7
marianodsn <58017977+marianodsn@users.noreply.github.com>
2023-08-31 14:43:32 -0400

    Add sneaker for in loop to generate messages

--- main.js
+++ main.js
@@ -5,6 +5,28 @@ const generateRandomNumber = () => Math.floor(Math.random() * 5);
 const sneakerOptions = {
     sneakerStyle: ['Jordan 1 Low', 'Jordan 1 High', 'Nike AirMax 1', 'Veja', 'Adidas Stan Smith'],
     occasion: ['casual walk', 'night out', 'work meeting', 'sneaker convention', 'sporting event'],
-    sneakerPurpose: ['compliment look', 'style pop', 'get comments', 'just because', 'new add']
+    sneakerPurpose: ['complimenting your look', 'creating a style pop', 'helping you get comments', 'because you have not worn them in a while', 'you should show off your new add']
 }
 
+// create array that will be used to display the message to user
+let sneakerSelection = []; 
+
+// save index selector for object iteration
+const indexSelect = generateRandomNumber()
+
+// iterate over the sneakerOptions object
+for (let prop in sneakerOptions) {
+    switch (prop) {
+        case 'sneakerStyle':
+            sneakerSelection.push(`You should wear your ${sneakerOptions.prop[indexSelect]} `);
+            break;
+        case 'occasion':
+            sneakerSelection.push(`They are a pretty good selection for a ${sneakerOptions.prop[indexSelect]} `);
+            break;
+        case 'sneakerPurpose':
+            sneakerSelection.push(`The also  ${sneakerOptions.prop[indexSelect]} `);
+            break;
+        default: 
+            sneakerSelection.push('There is not enough information.')
+    }
+}
